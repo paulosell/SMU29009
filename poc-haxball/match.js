5862da52202bd9b31f5c304024617c72661ece14
Paulo Sell <sellpaulof@gmail.com>
2019-10-06 16:06:51 -0300

    new visual

--- poc-haxball/match.js
+++ poc-haxball/match.js
@@ -5,47 +5,59 @@ export default class match extends Phaser.Scene {
 
     check_mov_player_one(){
         
+        if(this.cursor_one.space.isDown){
+            this.player_one.alpha = 1
+        }
         if (this.cursor_one.left.isDown){
-            this.player_one.setVelocityX(-550);       
+            this.player_one.setVelocityX(-150);       
         }      
 
         else if (this.cursor_one.right.isDown){
-            this.player_one.setVelocityX(250);      
+            this.player_one.setVelocityX(150);      
         }      
 
         else if (this.cursor_one.up.isDown){
-            this.player_one.setVelocityY(-250);      
+            this.player_one.setVelocityY(-150);      
         }      
 
         else if (this.cursor_one.down.isDown){
-            this.player_one.setVelocityY(250);   
+            this.player_one.setVelocityY(150);   
         }  
 
-        else if (this.cursor_one.left.isUp && this.cursor_one.right.isUp && this.cursor_one.up.isUp && this.cursor_one.down.isUp){
+        else if (this.cursor_one.space.isUp && this.cursor_one.left.isUp && this.cursor_one.right.isUp && this.cursor_one.up.isUp && this.cursor_one.down.isUp){
             this.player_one.setVelocity(0)
+            this.player_one.alpha = 0.8
         }
+
+        
+
 
     }
 
     check_mov_player_two(){
+
+        if(this.cursor_two.space.isDown){
+            this.player_two.alpha = 1
+        }
         if (this.cursor_two.left.isDown){
-            this.player_two.setVelocityX(-250);       
+            this.player_two.setVelocityX(-150);       
         }      
 
         else if (this.cursor_two.right.isDown){
-            this.player_two.setVelocityX(250);      
+            this.player_two.setVelocityX(150);      
         }      
 
         else if (this.cursor_two.up.isDown){
-            this.player_two.setVelocityY(-250);      
+            this.player_two.setVelocityY(-150);      
         }      
 
         else if (this.cursor_two.down.isDown){
-            this.player_two.setVelocityY(250);   
+            this.player_two.setVelocityY(150);   
         }  
 
-        else if (this.cursor_two.left.isUp && this.cursor_two.right.isUp && this.cursor_two.up.isUp && this.cursor_two.down.isUp){
+        else if (this.cursor_two.space.isUp && this.cursor_two.left.isUp && this.cursor_two.right.isUp && this.cursor_two.up.isUp && this.cursor_two.down.isUp){
             this.player_two.setVelocity(0)
+            this.player_two.alpha = 0.8
         }
 
     }
@@ -64,44 +76,106 @@ export default class match extends Phaser.Scene {
 
     preload()
     {
-
+        
+        
         this.load.image('bkg', 'assets/bkg.png');
-        this.load.image('limits','assets/limits.png')
-        this.load.image('goal-line', 'assets/goal-line.png')
+        this.load.image('ball', 'assets/ball.png')      
+        this.load.image('goal-net', 'assets/goal-net.png')
+        this.load.image('side-line', 'assets/side-line.png')
+        this.load.image('side-line-back', 'assets/side-line-back.png')
         this.load.image('mid-field', 'assets/mid-field.png')
-        this.load.image('ball', 'assets/ball.png')
-
-        this.load.spritesheet('dude', 
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    ); 
+        this.load.image('mid-field-circle', 'assets/mid-field-circle.png')
+        this.load.image('player-one', 'assets/player-one.png')
+        this.load.image('player-two', 'assets/player-two.png') 
 
 
+    }
+
+
+    players_collision(){
+        this.physics.add.collider(this.player_one, this.player_two);
+
+        this.physics.add.collider(this.player_one, this.side_line); 
+        this.physics.add.collider(this.player_two, this.side_line);
+        this.physics.add.collider(this.ball, this.side_line); 
+        
+        this.physics.add.collider(this.player_one,this.side_line_back)
+        this.physics.add.collider(this.player_two,this.side_line_back)
+        this.physics.add.collider(this.ball,this.side_line_back)
+
+        this.physics.add.collider(this.player_one,this.net)
+        this.physics.add.collider(this.player_two,this.net)
+        this.physics.add.collider(this.ball,this.net)
+
+        this.physics.add.overlap(this.player_one, this.goal_line); 
+        this.physics.add.overlap(this.player_two, this.goal_line); 
+        this.physics.add.overlap(this.ball, this.goal_line); 
+
+        
+
+    }
+
+
+    create_field(){
+
+        this.add.image(480,270,'bkg').setScale(0.5)
+        
+        this.side_line = this.physics.add.staticGroup()
+        this.side_line.create(480,477.5,'side-line').setScale(0.5).refreshBody();
+        this.side_line.create(480,62.5,'side-line').setScale(0.5).refreshBody();
+        
+        this.side_line_back = this.physics.add.staticGroup()
+
+        this.side_line_back.create(108.5,130.5,'side-line-back').setScale(0.5).refreshBody();
+        this.side_line_back.create(851.5,130.5,'side-line-back').setScale(0.5).refreshBody();
+        
+        this.side_line_back.create(108.5,409.5,'side-line-back').setScale(0.5).refreshBody();
+        this.side_line_back.create(851.5,409.5,'side-line-back').setScale(0.5).refreshBody();
+
+        this.goal_line = this.physics.add.staticGroup()
+        this.goal_line.create(108.5,270,'side-line-back').setScale(0.5).refreshBody();
+        this.goal_line.create(851.6,270,'side-line-back').setScale(0.5).refreshBody();
+
+        this.net = this.physics.add.staticGroup()
+        this.net.create(58.5,270,'side-line-back').setScale(0.5).refreshBody();
+        this.net.create(81,340.5,'goal-net').setScale(0.5).refreshBody();
+        this.net.create(81,199.5,'goal-net').setScale(0.5).refreshBody();
+
+        this.net.create(901.5,270,'side-line-back').setScale(0.5).refreshBody();
+        this.net.create(879,340.5,'goal-net').setScale(0.5).refreshBody();
+        this.net.create(879,199.5,'goal-net').setScale(0.5).refreshBody();
+
+        this.mid_field = this.physics.add.staticGroup()
+        this.mid_field.create(480,270,'mid-field').setScale(0.5).refreshBody();
+        this.mid_field.create(480,270,'mid-field-circle').setScale(0.5).refreshBody();
+
+       
+        
     }
 
     create()
     {
         this.timedEvent = this.time.delayedCall(500, this.stop_ball, [], this);
         this.timedEvent.paused = true
+        this.create_field()
         
-        this.add.image(960,540,'bkg')
+        this.player_one = this.physics.add.sprite(240, 270, 'player-one');
+        this.player_one.setScale(0.5)
+        this.player_one.setCollideWorldBounds(true);  
+        this.player_one.alpha = 0.8
         
-        this.add.image(960,540,'mid-field')
-
-        this.goal_lines = this.physics.add.staticGroup();
-        this.goal_lines.create(220.5, 550, 'goal-line');
-        this.goal_lines.create(1685.5, 550, 'goal-line');
-
-        this.limits = this.physics.add.staticGroup()
-        this.limits.create(960,540,'limits')
-
-        this.ball = this.physics.add.image(600,200, 'ball')
         
-        this.player_one = this.physics.add.sprite(250, 250, 'dude');
-        this.player_one.setCollideWorldBounds(true);     
+        this.player_two = this.physics.add.sprite(720, 270, 'player-two');
+        this.player_two.setCollideWorldBounds(true);   
+        this.player_two.setScale(0.5)   
+        this.player_two.alpha = 0.8
 
-        this.player_two = this.physics.add.sprite(850, 250, 'dude');
-        this.player_two.setCollideWorldBounds(true);        
+        this.ball = this.physics.add.image(480,270, 'ball')
+        this.ball.setScale(0.5)
+              
+        this.players_collision()
+
+   
 
         this.cursor_one = this.input.keyboard.createCursorKeys();
         this.cursor_two = this.input.keyboard.addKeys(
@@ -109,16 +183,16 @@ export default class match extends Phaser.Scene {
                 'left' : Phaser.Input.Keyboard.KeyCodes.A,
                 'right': Phaser.Input.Keyboard.KeyCodes.D, 
                 'up'   : Phaser.Input.Keyboard.KeyCodes.W, 
-                'down' : Phaser.Input.Keyboard.KeyCodes.S 
+                'down' : Phaser.Input.Keyboard.KeyCodes.S,
+                'space': Phaser.Input.Keyboard.KeyCodes.C 
             }
             )
    
+            this.c_one_ball = this.physics.add.collider(this.player_one, this.ball, this.check_touching_ball, null,this);
+            this.c_two_ball = this.physics.add.collider(this.player_two, this.ball, this.check_touching_ball, null,this);
+           
+ 
         
-        this.c_playerone_limits = this.physics.add.collider(this.player_one, this.limits); 
-        this.c_playertwo_limits = this.physics.add.collider(this.player_two, this.limits); 
-        this.c_player = this.physics.add.collider(this.player_one, this.player_two);
-        this.c_one_ball = this.physics.add.collider(this.player_one, this.ball, this.check_touching_ball, null, this);
-        this.c_two_ball = this.physics.add.collider(this.player_two, this.ball, this.check_touching_ball,  null,this);
         }
 
     update()
