b4bf43a66a9ffed5bd582378d0b516b8a3d1e356
Paulo Sell <sellpaulof@gmail.com>
2019-10-25 15:35:31 -0300

    showing text

--- omegle/front/src/pages/text/index.js
+++ omegle/front/src/pages/text/index.js
@@ -3,21 +3,45 @@ import React, { Component } from 'react'
 import './styles.css'
 
 export default class Text extends Component{
+
+    state = {
+        inputText: "",
+        divText: "",
+    }
+
+    componentDidMount(){
+        this.setState({hasChange: false})
+    }
+    
+    handleChange  = (event) =>(
+        this.setState({inputText: event.target.value})
+        
+
+    )
+        
+
+    getText = () => (
+        
+       this.setState({divText: this.state.divText + '\n' + this.state.inputText, inputText: "" })
+      
+            
+    )
+     
+    
     render(){
        return (
             <div className='text-page'>
-                <div className='chat'>
-                    
+                <div className='chat' >{this.state.divText}
+               
                 </div>
                 <div className='actions'>
                     <button>Trocar</button>
                     &nbsp;
                     &nbsp; 
-                    <input type='text'/>
+                    <input type='text'  value={this.state.inputText} onChange={this.handleChange}/>
                     &nbsp;
                     &nbsp; 
-                    <button>Enviar</button> 
-                    
+                    <button onClick = {this.getText}>Enviar</button>                     
                 </div>
 
             </div>
